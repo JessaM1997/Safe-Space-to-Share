7d536921b8c371a0c34068fd35cdff6f9ff2c017
Jessa Marie M. <jessamariedcmortega24@gmail.com>
2026-04-05 10:14:55 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function showContact(){
+
+let contact = document.getElementById("contact-info");
+
+if(contact.style.display === "block"){
+
+contact.style.display = "none";
+
+}
+
+else{
+
+contact.style.display = "block";
+
+}
+
+}
